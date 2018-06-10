e0655d5310620153082e9cf50d1e6139b0677d20
Eduardo Prioli Novaes <eprioli@gmail.com>
2018-06-10 20:25:51 -0300

    Agora o email consegue enviar conteudo do tipo HTML.

--- func.py
+++ func.py
@@ -1,7 +1,10 @@
 # coding: utf-8
 # autor: Eduardo Prioli Novaes
 # Sexta-feira, Junho 8 de 2018
+import smtplib
 
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 from tkinter import filedialog, Tk
 import smtplib
 
@@ -22,23 +25,35 @@ def abre_arquivo(nome_arquivo):
 def bootstrapServer():
     server = smtplib.SMTP('smtp.gmail.com:587')
     server.starttls()
+
+    # server = smtplib.SMTP('localhost')
     return server
 
+return server
+
 def enviar_email(destinatario, remetente, senha, server):
-    # Informações da mensagem
-    destinatario = destinatario
-    assunto      = 'Enviando email com python'
-    texto        = 'Esse email foi enviado usando python! :)'
+    # Informações da mensagem  - o tipo correto do MIME é multipart/alternative.
+    msg = MIMEMultipart('alternative')
+    msg['Subject'] = "Notificação para se cadastrar no Eduroam da UFRJ"
+    msg['From'] = remetente
+    msg['To'] = destinatario
 
-    # Preparando a mensagem
-    msg = '\r\n'.join([
-      'From: %s' % remetente,
-      'To: %s' % destinatario,
-      'Subject: %s' % assunto,
-      '',
-      '%s' % texto
-      ])
-
-    # Enviando o email
+    # Cria o corpo da mensagem.
+    text = "Usuário!\nPor favor clique no link abaixo para registrar sua senha para poder usar o Eduroam da UFRJ\nhttp://localhost:8000/eduroam/hit887FDD2FFD8E9FC270217AECC89D25EC76331118\n"
+    html = """\
+    <html>
+        <head></head>
+        <body>
+            <p>Usuário,<br>
+            Use o link abaixo para conectar-se ao nosso site para cadastrar sua senha no Eduroam.<br>
+            Aqui está o link do  <a href="http://localhost:8000/eduroam/hit887FDD2FFD8E9FC270217AECC89D25EC76331118">Eduroam</a> para cadastrar a sua senha.
+            </p>
+        </body>
+    </html>
+    """
+    part1 = MIMEText(text, 'plain')
+    part2 = MIMEText(html, 'html')
+    msg.attach(part1)
+    msg.attach(part2)
     server.login(remetente,senha)
-    server.sendmail(remetente, destinatario, msg)
+    server.sendmail(remetente, destinatario, msg.as_string())
--- projeto.py
+++ projeto.py
@@ -1,13 +1,18 @@
 # coding: utf-8
 # autor: Eduardo Prioli Novaes
 # Sexta-feira, Junho 8 de 2018
+#!/usr/bin/env python3
 
 # Esse projeto é para envio de emails com Python
 # Vai ler de um arquivo txt os endereços de emails e irá enviar um email de
 # notificação para cada um deles.
 
+
+
 import smtplib
 
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 from func import *
 from credenciais import *
 
@@ -15,13 +20,14 @@ from credenciais import *
 remetente    = getLogin()
 senha        = getSenha()
 
+
 def main():
     nome_arquivo = pede_arquivo()
     lista_emails = abre_arquivo(nome_arquivo)
     server = bootstrapServer()
     for destinatario in lista_emails:
         enviar_email(destinatario, remetente, senha, server)
-    server.close()
+    server.quit()
     print('Foi enviado notificação via email para os seguintes endereços eletrônicos:')
     print(lista_emails)
 
